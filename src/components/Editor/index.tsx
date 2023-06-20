6fb743c44740c9e3082993103cd9ef8925cae1ea
nauxscript <nauxscript@gmail.com>
2023-06-20 16:18:28 +0800

    feat: add ink-mde editor to support markdown format text
    sss

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,46 @@
+import { createSignal } from 'solid-js'
+import type { Instance } from 'ink-mde'
+import { Appearance, defineOptions, ink } from 'ink-mde'
+
+export interface EditorProps {
+  content?: string
+  onChange: (content: string) => void
+}
+
+const Editor = (props: EditorProps) => {
+  const [editorRef, setEditorRef] = createSignal<HTMLDivElement>()
+  const [editorClass, setEditorClass] = createSignal<Instance>()
+
+  const editorCustomStyle = {
+    '--ink-block-background-color': '#f9fafb',
+  }
+
+  const editorOptions = defineOptions({
+    doc: props.content || undefined,
+    files: {
+      clipboard: true,
+    },
+    hooks: {
+      afterUpdate: (doc: string) => {
+        props.onChange(doc)
+      },
+    },
+    interface: {
+      appearance: Appearance.Light,
+      attribution: false,
+      toolbar: false,
+      lists: true,
+    },
+    vim: true,
+  })
+
+  const handleInitEditor = (el: HTMLDivElement) => {
+    setEditorRef(el)
+    const editor = ink(el, editorOptions)
+    setEditorClass(editor)
+  }
+
+  return <div ref={handleInitEditor} class="ink-mde-editor" w-full h-full style={editorCustomStyle}></div>
+}
+
+export default Editor
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
 import MainLayout from '@/layouts/MainLayout'
+import Editor from '@/components/Editor/index'
 
 export default function Home() {
+  const handleEditorContentChange = (content: string) => {
+    // eslint-disable-next-line no-console
+    console.log(content)
+  }
   return (
-    <MainLayout></MainLayout>
+    <MainLayout editor={<Editor onChange={handleEditorContentChange} />}></MainLayout>
   )
 }
