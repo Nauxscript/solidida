f524e31d63bad7a6ddba047a1e6bdeeb22f74acb
nauxscript <nauxscript@gmail.com>
2023-06-20 16:18:58 +0800

    chore: adjust style

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,22 @@
+import type { JSX } from 'solid-js'
 import useDividerDrag from '@/hooks/useDividerDrag'
 
-export default function MainLayout() {
+export default function MainLayout(props: {
+  editor: JSX.Element
+}) {
   const [leftEleRef, setLeftEleRef] = createSignal<HTMLDivElement>()
   const [rightEleRef, setRightEleRef] = createSignal<HTMLDivElement>()
   const [centerEleRef, setCenterEleRef] = createSignal<HTMLDivElement>()
   const { handleLeftDividerDrag, handleRightDividerDrag } = useDividerDrag(leftEleRef, rightEleRef, centerEleRef)
   return (
     <div flex w-full h-full>
-      <div ref={el => setLeftEleRef(el)} w-300px>archive</div>
+      <div ref={el => setLeftEleRef(el)} w-300px overflow-y-auto>archive</div>
       <div w-2px bg-gray-1 hover:bg-blue-1 cursor-w-resize onMouseDown={handleLeftDividerDrag}></div>
-      <div ref={el => setCenterEleRef(el)} flex-1>Tasks</div>
+      <div ref={el => setCenterEleRef(el)} flex-1 overflow-y-auto>Tasks</div>
       <div w-2px bg-gray-1 hover:bg-blue-1 cursor-w-resize onMouseDown={handleRightDividerDrag}></div>
-      <div ref={el => setRightEleRef(el)} w-400px>editor</div>
+      <div ref={el => setRightEleRef(el)} w-400px overflow-y-auto>
+        {props.editor}
+      </div>
     </div>
   )
 }
--- src/root.tsx
+++ src/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
         <Meta name="description" content="Opinionated Vite Starter Template" />
         <link rel="icon" href="./favicon.svg"/>
       </Head>
-      <Body bg-gray-200>
+      <Body>
         <Suspense>
           <ErrorBoundary>
             <BaseLayout>
