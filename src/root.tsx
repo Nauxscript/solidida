ccf60e2178da57d7266495fcd294a82a6399edd6
nauxscript <nauxscript@gmail.com>
2023-06-21 14:16:51 +0800

    chore: change title

--- src/root.tsx
+++ src/root.tsx
@@ -12,7 +12,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>Vitesse-lite-solid</Title>
+        <Title>Solidida</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="description" content="Opinionated Vite Starter Template" />
