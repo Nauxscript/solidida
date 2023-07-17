b48070bd43fadd060885b9eb3d97d61a34cb3c64
nauxscript <nauxscript@gmail.com>
2023-07-17 10:27:25 +0800

    fix: type error

---
+++ shims.d.ts
@@ -0,0 +1,10 @@
+import type { AttributifyAttributes } from 'unocss'
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface HTMLAttributes<T> extends AttributifyAttributes {
+      flex?: boolean
+      rounded?: boolean
+    }
+  }
+}
