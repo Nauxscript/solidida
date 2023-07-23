a8a9fe14be7be91ad296a7c563504a1374e9dbe8
nauxscript <nauxscript@gmail.com>
2023-07-23 11:07:46 +0800

    refactor: archive panel

--- src/components/Archive/index.tsx
+++ src/components/Archive/index.tsx
@@ -1,3 +1,4 @@
+// import List from '../Listd'
 import List from '../List'
 import type { ListItemId } from '../ListItem'
 import ToggleButton from '../ToggleButton'
@@ -13,30 +14,45 @@ const Archive = (props: ArchiveProps) => {
 
   const [activedKey, setActivedKey] = createSignal<ListItemId>('')
 
-  const handleItemClick = (id: ListItemId) => {
-    setActivedKey(id)
+  const handleSelect = (key: number | string) => {
+    // eslint-disable-next-line no-console
+    console.log(key)
+    setActivedKey(key)
   }
 
   return (
     <section w-full h-full flex-col-box>
       <section>
-        <List data={smartProjects()} activeKey={activedKey()} itemClick={handleItemClick}></List>
+        <List.Root onSelect={handleSelect}>
+          <For each={smartProjects()} fallback={'loading'}>
+            {item => (<List.Item icon={item.icon} showOption={item.showOption} count={item.count}>{item.title}</List.Item>)}
+          </For>
+        </List.Root>
+
       </section>
       <div h-1px bg-gray-1 mx-2 my-2></div>
       <div class="mainProjectView" flex-col-box w-full px-2 box-border>
-        {
-          mainProjects().map(item => (
-            <>
-              <ToggleButton {...item}>
-                <List data={item.children} activeKey={activedKey()} itemClick={handleItemClick}></List>
-              </ToggleButton>
-            </>
-          ))
-        }
+        <For each={mainProjects()} fallback={'loading'}>
+          {(item, index) => (
+            <ToggleButton id={item.id} index={index} title={item.title}>
+              <List.Root onSelect={handleSelect}>
+                <For each={item.children}>
+                  {listItem => (<List.Item>{listItem.title}</List.Item>)}
+                </For>
+              </List.Root>
+            </ToggleButton>
+          )}
+
+        </For>
       </div>
       <div h-1px bg-gray-1 mx-2 my-2></div>
       <section>
-        <List data={concludedProjects()} activeKey={activedKey()} itemClick={handleItemClick}></List>
+        <List.Root onSelect={handleSelect}>
+          <For each={concludedProjects()} fallback={'loading'}>
+            {item => (<List.Item icon={item.icon} id={item.id}>{item.title}</List.Item>)}
+          </For>
+        </List.Root>
+        {/* <List data={concludedProjects()} activeKey={activedKey()} itemClick={handleItemClick}></List> */}
       </section>
       <div class="concludedProjectView"></div>
     </section>
