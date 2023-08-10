174686bf3c6071535d5c62c97f271482502c8c96
nauxscript mac-mini <nauxscript@gmail.com>
2023-08-10 22:46:30 +0800

    wip: should not add task when have no active project

--- src/store/tests/tasks.spec.ts
+++ src/store/tests/tasks.spec.ts
@@ -1,6 +1,7 @@
 import { describe } from 'vitest'
 import { createRoot } from 'solid-js'
 import { useTasksStore } from '../tasks'
+import { useTasksSelectorStore } from './../taskSelector'
 describe('Task Store', () => {
   beforeEach(() => {
     createRoot(() => {
@@ -16,6 +17,19 @@ describe('Task Store', () => {
       const task = addTask(taskTitle)
       expect(task.title).toBe(taskTitle)
       expect(tasks[tasks.length - 1].title).toBe(taskTitle)
+    })
+  })
+
+  test.todo('should not add task when have no active project', () => {
+    createRoot(() => {
+      const setCurrentSeletor = useTasksSelectorStore(state => state.setCurrentSeletor)
+      setCurrentSeletor(null)
+      const tasks = useTasksStore(state => state.tasks)
+      const addTask = useTasksStore(state => state.addTask)
+      const taskTitle = '吃饭'
+      const task = addTask(taskTitle)
+      expect(task).toBeUndefined()
+      expect(tasks.length).toBe(0)
     })
   })
 
