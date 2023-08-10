b60fd274314a2f4bb4cb0b3ff330cce451d3ab17
nauxscript <nauxscript@gmail.com>
2023-08-10 10:28:08 +0800

    feat: showZero prop of item to support hide the zero count

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -7,6 +7,7 @@ export interface ListItemProps {
   icon?: string
   showOption?: boolean
   count?: number
+  showZero?: boolean
 }
 
 export const OPTION_ICON_NAME = 'i-carbon-overflow-menu-horizontal'
@@ -27,13 +28,19 @@ export const ListItem: ParentComponent<ListItemProps> = (props) => {
     return context.activedKey() === defaultProps.id
   })
 
+  const normalizeCount = (count: number) => {
+    if (count || defaultProps.showZero)
+      return count
+    return ''
+  }
+
   return (
     <li class='group' flex items-center text-sm lh-10 font-normal list-none px-3 rounded cursor-pointer hover:bg-blue-50 classList={{ '!bg-blue-1': isActived() }} onClick={handleClick}>
       { defaultProps.icon && <i class={defaultProps.icon} text-gray-4></i>}
       <div class="content" px-2 flex-1>
         {defaultProps.children}
       </div>
-      <span data-testid='ttt' classList={{ 'group-hover:hidden': defaultProps.showOption, 'flex': defaultProps.count !== undefined }} text-gray-4 px-2 >{defaultProps.count}</span>
+      <span data-testid='ttt' classList={{ 'group-hover:hidden': defaultProps.showOption, 'flex': defaultProps.count !== undefined }} text-gray-4 px-2 >{normalizeCount(defaultProps.count)}</span>
       <i classList={{ 'group-hover:!flex': defaultProps.showOption, [OPTION_ICON_NAME]: true }} hidden text-gray-4 hover:text-gray-5 text-5></i>
     </li>
   )
