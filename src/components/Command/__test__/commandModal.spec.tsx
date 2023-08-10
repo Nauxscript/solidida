a1ccd385f1fe98701e228ae6dee1f4346ab0c347
nauxscript mac-mini <nauxscript@gmail.com>
2023-08-10 21:29:32 +0800

    refactor: command modal test

--- src/components/Command/__test__/commandModal.spec.tsx
+++ src/components/Command/__test__/commandModal.spec.tsx
@@ -7,53 +7,57 @@ describe('Command Modal', () => {
     closeCommandModal()
     // clear platform mock
     mockPlatform('')
+    // eslint-disable-next-line no-console
+    console.log('beforeEach')
   })
-  test('open command modal', () => {
-    const { openCommandModal, commandModalVisible } = useCommandModal()
-    openCommandModal()
-    expect(commandModalVisible()).toBe(true)
-  })
-
-  test('close command modal', () => {
-    const { openCommandModal, closeCommandModal, commandModalVisible } = useCommandModal()
-    openCommandModal()
-    closeCommandModal()
-    expect(commandModalVisible()).toBe(false)
-  })
-
-  test('cmd + k to ativate command modal on MacOS', async () => {
-    await createRoot(async (dispose) => {
-      mockPlatform('MacOS')
-      const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
-      const user = userEvent.setup()
-      registerKeyboardShortcut()
-      await user.keyboard('{Meta>}k/')
+  describe('basic control of command modal', () => {
+    test('open command modal', () => {
+      const { openCommandModal, commandModalVisible } = useCommandModal()
+      openCommandModal()
       expect(commandModalVisible()).toBe(true)
-      dispose()
     })
-  })
 
-  test('ctrl + k to ativate command modal on Window', async () => {
-    await createRoot(async (dispose) => {
-      mockPlatform('Window')
-      const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
-      const user = userEvent.setup()
-      registerKeyboardShortcut()
-      await user.keyboard('{Control>}k/')
-      expect(commandModalVisible()).toBe(true)
-      dispose()
-    })
-  })
-
-  test('cmd + k to ativate command modal and trigger this shorcut again will close the modal', async () => {
-    await createRoot(async () => {
-      mockPlatform('MacOS')
-      const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
-      const user = userEvent.setup()
-      registerKeyboardShortcut()
-      await user.keyboard('{Meta>}k/')
-      await user.keyboard('{Meta>}k/')
+    test('close command modal', () => {
+      const { openCommandModal, closeCommandModal, commandModalVisible } = useCommandModal()
+      openCommandModal()
+      closeCommandModal()
       expect(commandModalVisible()).toBe(false)
+    })
+
+    test('cmd + k to ativate command modal on MacOS', async () => {
+      await createRoot(async (dispose) => {
+        mockPlatform('MacOS')
+        const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
+        const user = userEvent.setup()
+        registerKeyboardShortcut()
+        await user.keyboard('{Meta>}k/')
+        expect(commandModalVisible()).toBe(true)
+        dispose()
+      })
+    })
+
+    test('ctrl + k to ativate command modal on Window', async () => {
+      await createRoot(async (dispose) => {
+        mockPlatform('Window')
+        const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
+        const user = userEvent.setup()
+        registerKeyboardShortcut()
+        await user.keyboard('{Control>}k/')
+        expect(commandModalVisible()).toBe(true)
+        dispose()
+      })
+    })
+
+    test('cmd + k to ativate command modal and trigger this shorcut again will close the modal', async () => {
+      await createRoot(async () => {
+        mockPlatform('MacOS')
+        const { registerKeyboardShortcut, commandModalVisible } = useCommandModal()
+        const user = userEvent.setup()
+        registerKeyboardShortcut()
+        await user.keyboard('{Meta>}k/')
+        await user.keyboard('{Meta>}k/')
+        expect(commandModalVisible()).toBe(false)
+      })
     })
   })
 })
