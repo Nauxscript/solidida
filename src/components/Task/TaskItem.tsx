b619f984e56528d41e36e6a8e88c25fa036cf6d4
nauxscript <nauxscript@gmail.com>
2023-08-22 15:52:47 +0800

    fix: check box status follow task status

--- src/components/Task/TaskItem.tsx
+++ src/components/Task/TaskItem.tsx
@@ -1,5 +1,6 @@
 import { Checkbox } from '@kobalte/core'
 import type { Component } from 'solid-js'
+import { TaskStatus } from '@/store'
 
 export interface TaskItemProps {
   title: string
@@ -7,12 +8,13 @@ export interface TaskItemProps {
   onContextMenu?: (e: MouseEvent) => void
   onClick?: (e: MouseEvent) => void
   isActived?: boolean
+  taskStatus: TaskStatus
 }
 
 export const TaskItem: Component<TaskItemProps> = (props) => {
   return (
     <div classList={{ '!bg-blue-1': props.isActived }} my-1px px-2 cursor-default flex-both-center w-full h-10 hover:bg-blue-50 onContextMenu={props.onContextMenu} onClick={props.onClick}>
-      <Checkbox.Root class="flex-both-center p-1 h-full box-border cursor-pointer" onChange={props.onChange}>
+      <Checkbox.Root class="flex-both-center p-1 h-full box-border cursor-pointer" onChange={props.onChange} defaultChecked={props.taskStatus === TaskStatus.COMPLETED}>
         <Checkbox.Input />
         <Checkbox.Control class="h-3 w-3 border-gray-4 border-1 border-solid flex-both-center rd-2px shadow-gray-2 shadow-sm shadow-inner text-3 text-white data-[checked]:bg-gray-4">
           <Checkbox.Indicator>
